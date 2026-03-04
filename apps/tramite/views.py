617fdd45143901dac7703e1f66d11d55fd3ce21b
PabloJunior12 <pablo_joseph01@hotmail.com>
2026-03-04 12:18:31 -0500

    tramite - update

--- apps/tramite/views.py
+++ apps/tramite/views.py
@@ -213,14 +213,6 @@ class ProcedureVirtualCreateAPIView(APIView):
 
     def post(self, request):
 
-        return Response(
-            {
-                "error": "Servicio en mantenimiento, El registro de procedimientos se encuentra temporalmente en mantenimiento. Intente nuevamente más tarde.",
-                "message": "El registro de procedimientos se encuentra temporalmente en mantenimiento. Intente nuevamente más tarde."
-            },
-            status=status.HTTP_503_SERVICE_UNAVAILABLE
-        )
-
         serializer = ProcedureCreateSerializer(
             data=request.data,
             context={"request": request}
@@ -302,14 +294,6 @@ class ProcedureCodePreviewAPIView(APIView):
 class ProcedureCreateAPIView(APIView):
 
     def post(self, request):
-
-        return Response(
-            {
-                "error": "Servicio en mantenimiento, El registro de procedimientos se encuentra temporalmente en mantenimiento. Intente nuevamente más tarde.",
-                "message": "El registro de procedimientos se encuentra temporalmente en mantenimiento. Intente nuevamente más tarde."
-            },
-            status=status.HTTP_503_SERVICE_UNAVAILABLE
-        )
 
         serializer = ProcedureCreateSerializer(
             data=request.data,
